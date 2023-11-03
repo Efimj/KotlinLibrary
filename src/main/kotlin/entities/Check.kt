14e8e183a96d23247e9634b763db0998b4f948c4
Dariia <d.o.prybyla@student.khai.edu>
2023-11-03 13:38:53 +0200

    Изменение классов Check и CheckStorage

--- src/main/kotlin/entities/Check.kt
+++ src/main/kotlin/entities/Check.kt
@@ -1,5 +1,11 @@
 package entities
 
+import java.util.Date
+
 data class Check(
     val checkId: Int,
+    val bookId: Long,
+    val userId: Long,
+    val price: Double,
+    val timestamp: Date
 )
--- src/main/kotlin/storages/CheckStorage.kt
+++ src/main/kotlin/storages/CheckStorage.kt
@@ -1,4 +1,25 @@
 package storages
 
+import entities.Check
 class CheckStorage {
+    private val checks = mutableListOf<Check>()
+
+    fun addCheck(check: Check) {
+        checks.add(check)
+    }
+
+    fun getCheckById(checkId: Int): Check? {
+        return checks.find { it.checkId == checkId }
+    }
+
+    fun getAllChecks(): List<Check> {
+        return checks.toList()
+    }
+
+    fun deleteCheck(checkId: Int) {
+        val check = checks.find { it.checkId == checkId }
+        check?.let {
+            checks.remove(it)
+        }
+    }
 }
